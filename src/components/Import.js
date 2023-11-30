b0837f396dcb6d099edb4acf830369e20706714a
Stewart Wingfield <sww915@gmail.com>
2023-11-30 14:40:31 -0500

    finished

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Container from "@mui/material/Container";
+import { MoreVert } from "@mui/icons-material";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const Import = (props) => {
-    // fill out this component
+  console.log("props", props);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleDelete = () => {
+    props.deleteMake();
+    setAnchorEl(null);
+  };
+  return (
+    <Container sx={{ marginTop: "40px" }}>
+      <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+        Import
+      </Button>
+      <h2>{`COUNT: ${props.makes.length}`}</h2>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
 
-    return (
-        <p>Import Component</p>
-    )
-}
+        <TableBody>
+          {props.makes.map((make) => (
+            <TableRow key={make.MakeId}>
+              <TableCell>{make.MakeId}</TableCell>
+              <TableCell>{make.MakeName}</TableCell>
+              <TableCell>
+                <Button
+                  id="basic-button"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MoreVert />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
-export default Import
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+      </Menu>
+    </Container>
+  );
+};
+
+export default Import;
