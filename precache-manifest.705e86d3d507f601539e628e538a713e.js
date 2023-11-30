16d279fce924c1af8d3d116ff6a7f6b96c302090
Stewart Wingfield <sww915@gmail.com>
2023-11-30 14:41:57 -0500

    Updates

---
+++ precache-manifest.705e86d3d507f601539e628e538a713e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4be824fc325d039e2ec77c81b3a20b",
+    "url": "/411reduxthunk/index.html"
+  },
+  {
+    "revision": "388c2654fe5bdc3b5747",
+    "url": "/411reduxthunk/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "e480b0ac1dd17d1ce3a9",
+    "url": "/411reduxthunk/static/js/2.5e051f46.chunk.js"
+  },
+  {
+    "revision": "388c2654fe5bdc3b5747",
+    "url": "/411reduxthunk/static/js/main.a1e95faf.chunk.js"
+  },
+  {
+    "revision": "3ab285867f52e3c3709d",
+    "url": "/411reduxthunk/static/js/runtime~main.071adfeb.js"
+  }
+]);
